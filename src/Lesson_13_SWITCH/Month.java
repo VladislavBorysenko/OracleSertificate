53754353c86b8eb34246e71390bc96d5a880a739
Vladislav <legionyipa@gmail.com>
2020-06-06 11:02:29 +0300

    for

--- src/Lesson_13_SWITCH/Month.java
+++ src/Lesson_13_SWITCH/Month.java
@@ -11,42 +11,22 @@ public class Month {
 
         Month mt = new Month(2);
         switch (mt.month){
-            case 1:
-                System.out.println("Yanvar 31 den");
-                break;
             case 2:
-                System.out.println("Fevral 28 dney");
-                break;
-            case 3:
-                System.out.println("Mart 31 den");
+                System.out.println("Количество дней в месяце 28");
                 break;
             case 4:
-                System.out.println("Aprel 30 dney");
-                break;
-            case 5:
-                System.out.println("May 31 den");
-                break;
             case 6:
-                System.out.println("Iyn 30 dney");
-                break;
-            case 7:
-                System.out.println("Iyl 31 den");
-                break;
-            case 8:
-                System.out.println("Avgust 31 den");
-                break;
             case 9:
-                System.out.println("Sentyabr 30 dney");
-                break;
-            case 10:
-                System.out.println("Oktyabr 31 den");
-                break;
             case 11:
-                System.out.println("Noyabr 30 dney");
+                System.out.println("Количество дней в месяце 30");
                 break;
-            case 12:
-                System.out.println("Dekabr 31 den");
-                break;
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+                System.out.println("Количество дней в месяце 31");
             default:
                 System.out.println("Takogo mesaca ne suchestvuet");
         }
---
+++ src/Lesson_14_for/Home/Test1.java
@@ -0,0 +1,4 @@
+package Lesson_14_for.Home;
+
+public class Test1 {
+}

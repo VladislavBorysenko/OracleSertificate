9a5b32be081fd974974df0685380a48011abac75
Vladislav <legionyipa@gmail.com>
2020-06-07 11:10:12 +0300

    Str methods

--- src/Lesson_16_ClassString/Test3.java
+++ src/Lesson_16_ClassString/Test3.java
@@ -1,4 +1,22 @@
 package Lesson_16_ClassString;
 
 public class Test3 {
+
+     static class Employee{
+
+        double salary;
+        boolean isManager;
+
+        public Employee(double salary, boolean isManager) {
+            this.salary = salary;
+            this.isManager = isManager;
+        }
+    }
+    static class EmployeeTest{
+        public static void main(String[] args) {
+
+            Employee emp=new Employee(1000,true);
+            System.out.println("On manager ?"+emp.isManager+" Ego zarplata"+emp.salary);
+        }
+    }
 }
---
+++ src/Lesson_16_ClassString/Test5.java
@@ -0,0 +1,9 @@
+package Lesson_16_ClassString;
+
+public class Test5 {
+    public static void main(String[] args) {
+        String s="Privet ANDREY";
+        String s2=s.toLowerCase();
+        System.out.println(s2);
+    }
+}

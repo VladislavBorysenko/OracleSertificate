6ccf910db7400e98961afdcc8153851f881bde0a
Vladislav <legionyipa@gmail.com>
2020-06-09 21:11:36 +0300

    Varargs

---
+++ src/Lesson_20_varargs_forea/Test2.java
@@ -0,0 +1,18 @@
+package Lesson_20_varargs_forea;
+
+public class Test2 {
+    static void summa(String as, int... a) {
+        int summa = 0;
+        for (int i = 0; i < a.length; i++) {
+            summa += a[i];
+        }
+        System.out.println(summa + " " + as);
+    }
+
+    public void abc(int[]...array){
+
+    }
+    public static void main(String[] args) {
+        summa("load", 2, 4, 5);
+    }
+}

4ab22a3c8afb6d35dedfac358668b6d9cf43b75e
Vladislav <legionyipa@gmail.com>
2020-06-09 18:28:13 +0300

    Varargs

--- src/Lesson_18_ARRAY/Home/Sort.java
+++ src/Lesson_18_ARRAY/Home/Sort.java
@@ -3,28 +3,17 @@ package Lesson_18_ARRAY.Home;
 import java.util.Arrays;
 
 public class Sort {
-    public static void sort(int[] arr){
-        int min=0;
-        for (int i = 0; i <arr.length ; i++) {
-            if (arr[i]>min){
-                min=arr[i];
 
 
-                System.out.println(arr[i]);
-            }
-
-        }
-    }
-
-    public static void sort2(int[] arr){
+    public static void sort2(int[] arr) {
         Arrays.sort(arr);
-        for (int i = 0; i <arr.length ; i++) {
+        for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
     }
 
     public static void main(String[] args) {
-        int []array2={1,4,7,9,5,3,56};
+        int[] array2 = {1, 4, 7, 9, 5, 3, 56};
         sort2(array2);
     }
 }

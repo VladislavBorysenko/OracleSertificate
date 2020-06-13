999baa80b171f3a66a15ff90a3b316d64523c5df
Vladislav <legionyipa@gmail.com>
2020-06-13 13:45:24 +0300

    Varargs

---
+++ src/Lesson_20_varargs_forea/HomeWork/Home.java
@@ -0,0 +1,17 @@
+package Lesson_20_varargs_forea.HomeWork;
+
+public class Home {
+    public String[] abc(String[]... str) {
+        String[] str1;
+        for (String[] s:str
+             ) {
+            
+        }
+        return str1;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Lesson_20_varargs_forea/Test9.java
@@ -0,0 +1,12 @@
+package Lesson_20_varargs_forea;
+
+public class Test9 {
+    public static void main(String[] args) {
+        int []array1={3,1,-2,0};
+        int []array2={-9,1,5,2};
+        for (int a:array1
+             ) {a=10;
+            
+        }
+    }
+}

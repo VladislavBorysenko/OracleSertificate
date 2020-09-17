6668b2228cb714514cde3f1c2085aa167232a9e2
Vladislav <legionyipa@gmail.com>
2020-09-17 23:38:33 +0300

    Exceptions

--- src/Lessons_27_Exceptions/Test4.java
+++ src/Lessons_27_Exceptions/Test4.java
@@ -7,30 +7,27 @@ import java.io.IOException;
 
 public class Test4 {
     void abc() throws FileNotFoundException {
-        File file = new File("test9.txt");
-        FileInputStream fis = new FileInputStream(file);
-//        try {
-//
-//        } catch (FileNotFoundException e) {
-//            System.out.println("exception :" + e);
-//
-//        }
+
+     File file = new File("test9.txt");
+     FileInputStream fis = new FileInputStream(file);
+        System.out.println("OK");
     }
 
-    void def(){
-        System.out.println( "privet");
-        try {
-            abc();
-        } catch (FileNotFoundException e) {
-            System.out.println("bil poiman exception :"+e);
-        }
+    void def() throws FileNotFoundException {
+abc();
     }
 
     public static void main(String[] args) {
 
+        Test4 test = new Test4();
+        try {
+            test.def();
+        } catch (FileNotFoundException e) {
 
-        System.out.println("Vsem horoshego dnya");
-
-        System.out.println("prosto cod");
+            System.out.println("Exception poyman");
+            System.out.println(e);
+            System.out.println(e.getMessage());
+          // e.printStackTrace();
+        }
     }
 }
---
+++ src/Lessons_27_Exceptions/Test5.java
@@ -0,0 +1,12 @@
+package Lessons_27_Exceptions;
+
+public class Test5 {
+    static void abc(){
+        System.out.println("Rabotaet Method abc");
+        abc();
+    }
+
+    public static void main(String[] args) {
+        abc();
+    }
+}
